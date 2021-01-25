e2c7bebf480ee0327e8d416a29bc7dcd7c4e18f9
ximena815 <68305685+ximena815@users.noreply.github.com>
2021-01-24 23:03:39 -0500

    Add files via upload

---
+++ Desafio02.02/Desafio02.02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Desafio02._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entradas
+            Console.WriteLine("ingrese t: ");
+            double t = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese aGrados: ");
+            double aGrados = double.Parse(Console.ReadLine());
+
+            //Proceso
+            double a = aGrados * (Math.PI / 180.0);
+
+            double z = ((Math.Cos(a)) * t);
+
+            Console.WriteLine("z: " + z);
+
+            double y = Math.Sqrt(((t) * (t)) - ((z) * (z)));
+            Console.WriteLine("y: " + y);
+
+            double c = 180 - 90 - aGrados;
+            Console.WriteLine("c: " + c);
+
+        }
+    }
+}
